c223a479c169f2623030e3ceda1b25eb12a2056e
SK <edu.sk2279@gmail.com>
2020-06-09 17:17:43 +0900

    200609 || django

---
+++ django/intro/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!\\70.12.116.50\users\student\desktop\python\django\intro\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
